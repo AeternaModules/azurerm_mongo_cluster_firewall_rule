ab0eb49b9329bb82868c208d5e26b12d50e3b7b0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "mongo_cluster_firewall_rules" {
-  description = "All mongo_cluster_firewall_rule resources"
-  value       = azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules
-}
 output "mongo_cluster_firewall_rules_end_ip_address" {
-  description = "List of end_ip_address values across all mongo_cluster_firewall_rules"
-  value       = [for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : v.end_ip_address]
+  description = "Map of end_ip_address values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
+  value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.end_ip_address }
 }
 output "mongo_cluster_firewall_rules_mongo_cluster_id" {
-  description = "List of mongo_cluster_id values across all mongo_cluster_firewall_rules"
-  value       = [for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : v.mongo_cluster_id]
+  description = "Map of mongo_cluster_id values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
+  value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.mongo_cluster_id }
 }
 output "mongo_cluster_firewall_rules_name" {
-  description = "List of name values across all mongo_cluster_firewall_rules"
-  value       = [for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : v.name]
+  description = "Map of name values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
+  value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.name }
 }
 output "mongo_cluster_firewall_rules_start_ip_address" {
-  description = "List of start_ip_address values across all mongo_cluster_firewall_rules"
-  value       = [for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : v.start_ip_address]
+  description = "Map of start_ip_address values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
+  value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.start_ip_address }
 }
 
