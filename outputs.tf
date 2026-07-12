1427f69df1c2f483bbb10008ecf903a0bc582f3e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "mongo_cluster_firewall_rules_id" {
+  description = "Map of id values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
+  value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.id }
+}
 output "mongo_cluster_firewall_rules_end_ip_address" {
   description = "Map of end_ip_address values across all mongo_cluster_firewall_rules, keyed the same as var.mongo_cluster_firewall_rules"
   value       = { for k, v in azurerm_mongo_cluster_firewall_rule.mongo_cluster_firewall_rules : k => v.end_ip_address }
